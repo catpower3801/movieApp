ed0a520401055f95073699a13c7fd90cef001bd5
catpower3801 <gggoas999@naver.com>
2019-05-01 16:15:27 +0900

    complete movie app

--- src/App.js
+++ src/App.js
@@ -3,8 +3,30 @@ import './App.css';
 import Movie from './Movie';
 class App extends Component {
   state={}
+  _callApi = () =>{
+    //fetch() 자바스크립트로 외부의 url을 가져옴
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count').then(b=>b.json()).then(c=>c.data.movies).catch(err=>console.log(err));
+    //에러가 났을떄 catch
+  } 
+  //async : fetch로 받아온 데이터와 그걸 처리하는 then들을 더욱 간결하고 명료하게 만들어주는 도구
+  //fetch(url).then(a => a.json()).then............
+  //fetch와 then을 사용해서 일일이 모든 동작을 지정해주지 않고, fetch가 끝난 다음 바로 실행시킬 것들을 지정할 수 잇게 만드는 tool 
 
+  //await
+  //async : 비동기
+  //async : 이전라인 의 작업이 끝날때까지 기다리지 않고 실행 될 작업
+  _getMovies= async () => {
+    const movies = await this._callApi();
+    console.log(movies)
+    this.setState({
+      movies
+    })
+  }
   componentDidMount(){
+    this._getMovies();
+  }
+/*
+componentDidMount(){
     setTimeout(() => {
       this.setState({
         movies : [
@@ -28,10 +50,11 @@ class App extends Component {
       })
     },5000);
   }
+  */
   
   //랜더링 function
   _renderMovies = () =>{
-    const movies = this.state.movies.map((x,index)=>{return <Movie title={x.title} poster={x.poster} key={index}/>});
+    const movies = this.state.movies.map((x,index)=>{return <Movie title={x.title} poster={x.medium_cover_image} key={index} genres={x.genres} synopsis={x.synopsis}/>});
     return movies;
   }
  
@@ -42,6 +65,12 @@ class App extends Component {
         //this.state.movies가 있을땐 _renderMovies 실행
         //state가 비어있는 상황일땐 loading... 문구
         this.state.movies ? this._renderMovies() : 'Loading'
+        /*if(this.state.moveis){
+          this._renderMovies()
+        }else{
+          'Loading'
+        }
+        */
       }
       </div>
     );
--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,43 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
+import LinesEllipsis from 'react-lines-ellipsis'
+/*
+class Movie extends Component..
+장정:state를 쓸 수 있음
+    -componentDidMount(),
+    compontentWillMount().....
+단점:코드가 복잡해짐
 
+function Movie({props1,prosp2})
+함수형 컴포넌트(domb Component)
+단지 어떤 값을 return 해서 출력해주기만들
+위한 컴포넌트
+장점:코드가 단순
+단점:state가 없음
+*/
+function MovieGenres(genres){
+    return(
+        <span>{genres}</span>
+    )
+}
+function Movie({title,poster,genres,synopsis}){
+    return(
+        <div className="card">
+            <h1>{title}</h1>
+            <Movie_poster poster={poster}/>
+            <p>{/*genres.map((y,index) => <MovieGenres genres={y} key={index}/>)*/console.log(MovieGenres(genres))}</p>
+            <p><LinesEllipsis
+            text={synopsis}
+            maxLine='3'
+            ellipsis='...'
+            trimRight
+            basedOn='letters'
+            /></p>
+        </div>
+    )
+}
+/*
 class Movie extends Component{
     static propTypes = {
         title : PropTypes.string.isRequired,
@@ -16,7 +52,21 @@ class Movie extends Component{
         )
     }
 }
+*/
 
+/*
+component
+- props : 부모로부터 받아오는 값(읽기 전용)
+- state : 각각의 컴포넌트가 가지는 객체 string
+    state에 변경사항이 있을때 마다
+    component가 새로 랜더링 되기 떄문에!!
+*/
+function Movie_poster({poster}){
+    return(
+        <img src={poster} alt="Movie Poster"/>
+    )
+}
+/*
 class Movie_poster extends Component{
     static propTypes ={
         poster : PropTypes.string.isRequired
@@ -27,5 +77,13 @@ class Movie_poster extends Component{
         )
     }
 }
+*/
 
+Movie.propTypes={
+    title:PropTypes.string.isRequired,
+    poster:PropTypes.string.isRequired
+}
+Movie_poster.propTypes={
+    poster:PropTypes.string.isRequired
+}
 export default Movie;
